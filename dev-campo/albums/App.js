5fa57c7552f71920e57130763cc49c5692d6a392
Aaron Goodman <amgoodman5@gmail.com>
2017-10-23 21:56:21 -0600

    rendering list

--- dev-campo/albums/App.js
+++ dev-campo/albums/App.js
@@ -11,12 +11,16 @@ import {
   View
 } from 'react-native';
 import Header from './src/components/header'
+import AlbumList from './src/components/AlbumList'
 
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Header/>
+      <View>
+      <Header headerText = {'Albums'}/>
+      <AlbumList/>
+      </View>
     );
   }
 }
