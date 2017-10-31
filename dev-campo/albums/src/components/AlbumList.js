38149307403c155157a98821bfde926eda536991
Aaron Goodman <amgoodman5@gmail.com>
2017-10-31 15:26:10 -0600

    thumbnail

--- dev-campo/albums/src/components/AlbumList.js
+++ dev-campo/albums/src/components/AlbumList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
-
+import {View} from 'react-native'
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
   state = {albums: []};
@@ -12,13 +12,13 @@ class AlbumList extends Component {
   }
 
   renderAlbums(){
-  return this.state.albums.map(album => <Text> {album.title}</Text>)
+  return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />)
   }
   render(){
          console.log(this.state)
    return (
      <View>
-      {this.renderAlbums()} 
+      {this.renderAlbums()}
      </View>
    );
   }
