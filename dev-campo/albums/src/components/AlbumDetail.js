ef5d532bea30ef50d4ccd7e2c17684950419bd09
Aaron Goodman <amgoodman5@gmail.com>
2017-10-31 18:11:54 -0600

    button component

--- dev-campo/albums/src/components/AlbumDetail.js
+++ dev-campo/albums/src/components/AlbumDetail.js
@@ -2,36 +2,62 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Buttons';
 
 const AlbumDetail = ({ album }) => {
-  const {title, artist, thumbnail_image} = album;
-  const {thumbnailStyle, headerContentStyle} = styles;
+  const {title, artist, thumbnail_image, image} = album;
+  const {thumbnailStyle,
+     headerContentStyle,
+     thumbnailContaierStyle,
+     headerTextStyle,imageStyle} = styles;
+
 return (
-  <Card>
-    <CardSection>
-      <View>
-        <Image
-        style ={thumbnailStyle}
-        source={{uri: thumbnail_image}}/>
-      </View>
-      <View style={headerContentStyle}>
-        <Text>{title}</Text>
-        <Text>{artist}</Text>
-      </View>
-    </CardSection>
-  </Card>
-  );
+    <Card>
+      <CardSection>
+        <View style={thumbnailContaierStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{uri: thumbnail_image}}
+            />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image source ={{uri:image}} style={imageStyle}/>
+      </CardSection>
+      <CardSection>
+      <Button/>
+      </CardSection>
+    </Card>
+    );
 };
 
 const styles = {
+  headerTextStyle:{
+    fontSize:18
+  },
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
-
   },
   thumbnailStyle:{
     height: 50,
     width:50
+  },
+  thumbnailContaierStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height:300,
+    flex: 1,
+    width:null
   }
 }
 export default AlbumDetail;
